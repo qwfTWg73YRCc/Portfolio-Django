57909fdf9476ec8b2f774969bd2745a438e2bb88
Konstantin <ektov_konstantin@mail.ru>
2021-02-25 11:03:12 +0300

    added .gitignore file

--- personal_portfolio/asgi.py
+++ personal_portfolio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for personal_portfolio project.
+ASGI config for personal_portfolio projects.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_portfolio project.
+WSGI config for personal_portfolio projects.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
