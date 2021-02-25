7831b27f99c17a63a023ed505a953d3a5ed22c5c
Konstantin <ektov_konstantin@mail.ru>
2021-02-25 14:46:53 +0300

    added __str__ method to Project class in models.py

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,6 @@ class Project(models.Model):
     technology = models.CharField(max_length=32)
     image = models.FilePathField(path="/img")
 
+    def __str__(self):
+        return f"{self.title}, {self.description}, {self.technology}, {self.image}"
+
