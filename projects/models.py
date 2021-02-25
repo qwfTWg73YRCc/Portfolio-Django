57137a58ff47013a5267f38c7806f9fbb58372fe
Konstantin <ektov_konstantin@mail.ru>
2021-02-25 11:58:49 +0300

    created models for projects and made migrations

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    technology = models.CharField(max_length=32)
+    image = models.FilePathField(path="/img")
+
